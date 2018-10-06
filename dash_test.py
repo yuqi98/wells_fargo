fe88f3cbde9ac1102fd1cefe2c2ca16e76b5b65e
Samuel Appiah-Kubi <sa285@duke.edu>
2018-10-06 00:08:34 -0400

    update on graphs

--- dash_test.py
+++ dash_test.py
@@ -4,23 +4,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State, Event
 import plotly.plotly as py
-from plotly.graph_objs import *
-from scipy.stats import rayleigh
+import plotly.tools as tls
 import plotly.graph_objs as go
 import numpy as np
 import pandas as pd
-import sqlite3
-import datetime as dt
 import base64
 
 #css dependency
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 #read in csv
-user_doc = pd.read_csv('first_preprocess.csv', index_col='user_num')
-np.random.seed(42)
-random_x = np.random.randint(1,101,100)
-random_y = np.random.randint(1,101,100)
+data = pd.read_csv('first_preprocess.csv', index_col='user_num')
 
 
 #image read in
@@ -44,58 +38,22 @@ styles = {
 app.layout = html.Div([
     html.H1('User Carbon FootPrint Statistics', style=styles['title']),
     html.Div(dcc.Input(id='input-box', type='text', style={'margin': '0px 0px 0px 5%'})),
-    html.Button('Submit', id='button', style={'margin': '0px 0px 25px 5%'}),
+    html.Button('User', id='button', style={'margin': '0px 0px 25px 5%'}),
     #User Details
     html.Div([html.Img(src=encode_image('user.png'), style={'height': '100px', 'boxShadow': '0px 0px 2px 2px rgba(204,204,204,0.4)',
                         'marginLeft': '15%', 'float': 'left'}),
             html.Div([html.P('Name: User1'), html.P('Location: Durham, North Carolina'), html.P('Area of most impact')], style={'padding': '2%', 'float': 'left'}),
+            #increase current margin
             html.Div('A', style=styles['user_det'])
             ],
             style={'columnCount': 3}),
     html.Div(style={'float': 'clear'}),
 
     #plotted graphs
-    html.Div([
-            dcc.Graph(id='group-visual',
-                figure={
-                    'data':[
-                        {'x': user_doc.columns, 'y': user_doc.iloc[1], 'type': 'bar',
-                        'align': 'center'}
-                    ],
-                    'layout': {'title': 'Group Performance'}
-            }),
-            dcc.Graph(
-                id='scatter3',
-                figure={
-                    'data': [
-                        go.Scatter(
-                            x = random_x,
-                            y = random_y,
-                            mode = 'markers',
-                            marker = {
-                                'size': 12,
-                                'color': 'rgb(51,204,153)',
-                                'symbol': 'pentagon',
-                                'line': {'width': 2}
-                                }
-                        )
-                    ],
-                    'layout': go.Layout(
-                        title = 'ScatterPlot of AggregateCFP',
-                        xaxis = {'title': 'Some random x-values'},
-                        yaxis = {'title': 'Some random y-values'},
-                        hovermode='closest'
-                    )
-                }
-            ),
-            dcc.Graph(id='third-visual',
-                        figure={
-                            'data':[
-                                {'x': user_doc.columns, 'y': user_doc.iloc[50], 'type': 'bar',
-                                'align': 'center'}
-                            ],
-                            'layout': {'title': 'Connections'}
-            })
+    html.Div(id="graphs", [
+            dcc.Graph(id='personal-visual'),
+            dcc.Graph(id='performance-chart'),
+            dcc.Graph(id='group-visual')
             ],
             style={'columnCount': 3}),
     #recommendations
@@ -103,12 +61,32 @@ app.layout = html.Div([
 
 ], style=styles['main'])
 
-# @app.callback(
-#     Output(component_id='user-id', component_property='children'),
-#     [Input(component_id='request-id', component_property='value')]
-# )
-# def update_output_div(input_value):
-#     return '{}'.format(input_value)
+@app.callback(
+    Output(component_id='graph', component_property='children'),
+    [Input(component_id='input-box', component_property='value')]
+)
+def update_output_div(input_value):
+    dat = data.copy()
+    dat.columns = ['heating','bath','kitchen','TV','transportation','waste']
+    df_pi = pd.DataFrame(index=data.index)
+    df_pi['waste'] = dat['waste']
+    df_pi['transportation'] = dat['transportation']
+    df_pi['other'] = dat['heating'] + dat['bath'] + dat['kitchen'] + dat['TV']
+    user = df_pi.iloc[input_value, :]
+    labels = df_pi.columns
+    values = user.values
+    colors = ['#6B8E23', '#9ACD32','#98FB98']
+    return figure={'data':[
+                        go.Pie(labels=labels, values=values,
+                               hoverinfo='label+percent', textinfo='value',
+                               hole=.3,
+                               textfont=dict(size=15),
+                               marker=dict(colors=colors,
+                               line=dict(color='#000000', width=0.5)))
+                    ],
+                    'layout': go.layout(
+                        'title' = 'Person CFP Contribution',
+                    )}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
